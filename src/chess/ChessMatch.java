3647eb1067d7b3e17742d4e7a86d944467d27a42
Henrico Bela <henrico.nardelli@hotmail.com>
2020-09-01 11:45:37 -0300

    King moves implemented

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -3,6 +3,7 @@ package chess;
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
+import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -70,7 +71,7 @@ public class ChessMatch {
        // placeNewPiece('b', 1, new Knight(board, Color.WHITE));
        // placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
        // placeNewPiece('d', 1, new Queen(board, Color.WHITE));
-       // placeNewPiece('e', 1, new King(board, Color.WHITE, this));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
        // placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
        // placeNewPiece('g', 1, new Knight(board, Color.WHITE));
        // placeNewPiece('h', 1, new Rook(board, Color.WHITE));
@@ -87,7 +88,7 @@ public class ChessMatch {
        // placeNewPiece('b', 8, new Knight(board, Color.BLACK));
        // placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
        // placeNewPiece('d', 8, new Queen(board, Color.BLACK));
-       // placeNewPiece('e', 8, new King(board, Color.BLACK, this));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
        // placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
        // placeNewPiece('g', 8, new Knight(board, Color.BLACK));
        // placeNewPiece('h', 8, new Rook(board, Color.BLACK));
